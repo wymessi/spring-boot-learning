45da5907ab33c56d63ddb8bf72424986eca1b1d1
ywang5 <ywang5@leqee.com>
2018-11-27 17:03:05 +0800

    demo of activemq

---
+++ src/main/java/com/wymessi/activemq/Consumer2.java
@@ -0,0 +1,12 @@
+package com.wymessi.activemq;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumer2 {
+    @JmsListener(destination = "publish.queue")
+    public void receiveQueue(String consumer) {
+        System.out.println(consumer+"消息已经消费了 BY QUEUE 消费者2");
+    }
+}
