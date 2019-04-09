c9044dd632f1588548f3b7521ac8a831b398fe66
wymessi <wangye_glqx@163.com>
2019-04-09 20:59:45 +0800

    t

--- spring-boot-schedule/src/main/java/com/wymessi/schedule/SchedulerTask.java
+++ spring-boot-schedule/src/main/java/com/wymessi/schedule/SchedulerTask.java
@@ -3,12 +3,10 @@ package com.wymessi.schedule;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * springboot自带的定时任务
@@ -17,14 +15,53 @@ import java.util.Date;
  *
  */
 @Component
-@Async //开启任务异步执行，多线程执行任务
+//@Async //开启任务异步执行，多线程执行任务
 public class SchedulerTask {
 
 	private static final Logger logger = LogManager.getLogger(SchedulerTask.class);
-	
+
+	AtomicInteger atomicInteger = new AtomicInteger(1);
+
+	/**
+	 * 按照指定频率执行任务，并以此规则开始周期性的执行调度。
+	 *
+	 * 例如：fixedRateTask()方法在第0秒开始执行，方法执行了12秒，那么下一次执行fixedRateTask()方法的时间是第12秒。。
+	 * @throws Exception
+	 */
 	@Scheduled(fixedRate=2000) //每两秒执行一次
-//	@Scheduled(cron = "0 0/1 * * * ? ")
-	public void task(){
-		logger.info("定时任务-现在时间：" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+	public void fixedRateTask() throws Exception{
+		int i = atomicInteger.get();
+		if (i < 5) {
+			Thread.sleep(i * 1000);
+		}
+		atomicInteger.getAndIncrement();
+		logger.info("fixedRate 定时类型");
+	}
+
+	@Scheduled(cron = "*/2 * * * * ? ")
+	public void cronTask() throws Exception{
+		int i = atomicInteger.get();
+		if (i < 5) {
+			Thread.sleep(i * 1000);
+		}
+		atomicInteger.getAndIncrement();
+		logger.info("cron 定时类型");
+	}
+
+
+	/**
+	 * 下一次的任务执行时间，是从方法最后一次任务执行结束时间开始计算。并以此规则开始周期性的执行任务。
+	 *
+	 * 例如：fixdelaytask()方法在第0秒开始执行，方法执行了12秒，那么下一次执行work()方法的时间是第14秒。
+	 * @throws Exception
+	 */
+	@Scheduled(fixedDelay = 2000)
+	public void fixdelayTask() throws Exception{
+		int i = atomicInteger.get();
+		if (i < 5) {
+			Thread.sleep(i * 1000);
+		}
+		atomicInteger.getAndIncrement();
+		logger.info("fixedDelay 定时类型");
 	}
 }
